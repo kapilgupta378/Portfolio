3a5ca0e4fcb6c4f57cdac69fbaeeccfb22e59d3e
kapil gupta <kapil0506gupta@gmail.com>
2021-07-20 05:53:53 -0600

    2nd update slide

--- src/components/work/Works.jsx
+++ src/components/work/Works.jsx
@@ -3,7 +3,14 @@ import "./work.scss"
 function Works() {
     return (
         <div className='works' id='works'>
-            
+           <div className="slider">
+               <div className="container">
+                   <div className="item">
+                       <div className="left"></div>
+                       <div className="right"></div>
+                   </div>
+               </div>
+           </div> 
         </div>  
     )
 }
