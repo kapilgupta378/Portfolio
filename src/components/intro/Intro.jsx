d32baaffa73bbbb514a425cd69075d2b84f1fa79
kapil gupta <kapil0506gupta@gmail.com>
2021-10-20 18:49:32 +0530

    added new banner

--- src/components/intro/Intro.jsx
+++ src/components/intro/Intro.jsx
@@ -1,39 +1,43 @@
 import "./intro.scss";
 import { init } from "ityped";
 import { useEffect, useRef } from "react";
-
+import img from "./kapilgupta.png";
+import video from "./videos/video3.mp4";
 function Intro() {
   const textRef = useRef();
 
   useEffect(() => {
     console.log(textRef);
-    
+
     init(textRef.current, {
       showCursor: true,
       backDelay: 1500,
       backSpeed: 60,
-      strings: ["Developer", "React", "Website"],
+      strings: ["Developer", "React dev.", "Front End Dev."],
     });
   }, []);
 
   return (
     <div className="intro" id="intro">
-      <div className="left">
-        <div className="imgContainer">
-          <img src="assets/man.png" alt="" />
+      <video className="video" src={video} autoPlay loop muted />
+      <div className="bannerWrap">
+        <div className="left">
+          <div className="imgContainer">
+            <img src={img} alt="" />
+          </div>
         </div>
-      </div>
-      <div className="right">
-        <div className="wrapper">
-          <h2>Hi there, I'm</h2>
-          <h1>Kapil Gupta</h1>
-          <h3>
-            Full Time <span ref={textRef}></span>
-          </h3>
+        <div className="right">
+          <div className="wrapper">
+            <h2>Hi there, I'm</h2>
+            <h1>Kapil Gupta</h1>
+            <h3>
+              Full Time <span ref={textRef}></span>
+            </h3>
+          </div>
+          <a href="#portfolio">
+            <img src="assets/down.png" alt="" />
+          </a>
         </div>
-        <a href="#portfolio">
-          <img src="assets/down.png" alt="" />
-        </a>
       </div>
     </div>
   );
--- src/components/work/Works.jsx
+++ src/components/work/Works.jsx
@@ -1,5 +1,6 @@
 import { useState } from "react";
 import "./work.scss";
+import ArrowBackIosIcon from "@material-ui/icons/ArrowBackIos";
 
 export default function Works() {
   const [currentSlide, setCurrentSlide] = useState(0);
@@ -8,28 +9,22 @@ export default function Works() {
       id: "1",
       icon: "./assets/mobile.png",
       title: "Web Design",
-      desc:
-        "Lorem Ipsum is simply dummy text of the printing and typesetting industry. ",
-      img:
-        "https://99designs-blog.imgix.net/blog/wp-content/uploads/2018/10/attachment_100040756-e1538485934255.jpeg?auto=format&q=60&fit=max&w=930",
+      desc: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. ",
+      img: "https://99designs-blog.imgix.net/blog/wp-content/uploads/2018/10/attachment_100040756-e1538485934255.jpeg?auto=format&q=60&fit=max&w=930",
     },
     {
       id: "2",
       icon: "./assets/globe.png",
       title: "Mobile Application",
-      desc:
-        "Lorem Ipsum is simply dummy text of the printing and typesetting industry.",
-      img:
-        "https://i.pinimg.com/originals/e9/c9/2f/e9c92f7869d682a6fa5a97fb8a298f30.jpg",
+      desc: "Lorem Ipsum is simply dummy text of the printing and typesetting industry.",
+      img: "https://i.pinimg.com/originals/e9/c9/2f/e9c92f7869d682a6fa5a97fb8a298f30.jpg",
     },
     {
       id: "3",
       icon: "./assets/writing.png",
       title: "Branding",
-      desc:
-        "Lorem Ipsum is simply dummy text of the printing and typesetting industry.",
-      img:
-        "https://i.pinimg.com/originals/a9/f6/94/a9f69465d972a004ad581f245d6ad581.jpg",
+      desc: "Lorem Ipsum is simply dummy text of the printing and typesetting industry.",
+      img: "https://i.pinimg.com/originals/a9/f6/94/a9f69465d972a004ad581f245d6ad581.jpg",
     },
   ];
 
@@ -38,7 +33,7 @@ export default function Works() {
       ? setCurrentSlide(currentSlide > 0 ? currentSlide - 1 : 2)
       : setCurrentSlide(currentSlide < data.length - 1 ? currentSlide + 1 : 0);
   };
-  
+
   return (
     <div className="works" id="works">
       <div
@@ -69,16 +64,16 @@ export default function Works() {
         ))}
       </div>
       <img
-        src="assets/arrow.png"
+        src={<ArrowBackIosIcon />}
         className="arrow left"
-        alt=""
+        alt="arrow"
         onClick={() => handleClick("left")}
       />
       <img
-        src="assets/arrow.png"
+        src="/assets/arrow.png"
         className="arrow right"
-        alt=""
-        onClick={() => handleClick()}
+        alt="arrow"
+        onClick={() => handleClick("right")}
       />
     </div>
   );
